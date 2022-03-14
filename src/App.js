ed174901046d038d9bdbcb45adb9ec34349083c8
ssourbh000 <86834692+ssourbh000@users.noreply.github.com>
2022-03-14 11:04:24 +0530

    text added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React,{ useEffect, useState } from 'react';
 import './App.css';
 import Recipe from './Recipe';
+import Text from './Text';
 
 function App() {
 
@@ -11,7 +12,7 @@ function App() {
 
   const [recipes, setRecipes] = useState([]);
   const [search, setSearch] = useState('');
-  const [query, setQuery] = useState('chicken');
+  const [query, setQuery] = useState('');
 
   useEffect( ()=>{
     getRecipe();
@@ -36,13 +37,20 @@ function App() {
 
   return (
     <div className="App">
+     
       <form onSubmit={getSearch} className="search-form">
-        <input type="text" className="search-bar" value={search} onChange={updateSearch} />
-        <button className='search-button' type="submit">
+        
+        <br />
+        <input type="text" className="search-bar" value={search} onChange={updateSearch} placeholder="search your today's dish" />
+        <button className='search-button' type="submit" style={{cursor: "pointer"}}>
         Search
         </button>
+        
       </form>
+
+      <Text />
       <div className='recipes'>
+        
         {recipes.map(recipe => (
           <Recipe 
           key={recipe.recipe.label}
---
+++ src/Text.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Text() {
+  return (
+    <div style={{display: "flex", flexDirection:"column" ,justifyContent: "center", alignItems:"center"}}>
+      <h1>Hey, let your Taste Buds test a new Recipe?</h1>
+      <img style={{width: "50%", height: "60vh", borderRadius: "50%"}} src="https://appgrooves.com/cdn/lifegoal/220/e/53014-45519-56524-56048_w1200.png" alt="food" />
+    </div>
+  )
+}
+
+export default Text
